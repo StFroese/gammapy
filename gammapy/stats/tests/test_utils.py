c17058ef6609d847e15e9d0b0ea3c77f17ce66f7
Stefan Fröse <stefan.froese@udo.edu>
2026-09-21 19:25:51 +0800

    correct asimov behaviour of ts_to_sigma/sigma_to_ts
    Signed-off-by: Stefan Fröse <stefan.froese@udo.edu>

--- gammapy/stats/tests/test_utils.py
+++ gammapy/stats/tests/test_utils.py
@@ -26,7 +26,12 @@ def test_sigma_ts_conversion():
     sigma_ref = 3
     ts_ref = 9
     df = 1
-    ts = sigma_to_ts(3, df=df, n_sigma_asimov=3)
+    ts = sigma_to_ts(3, df=df, asimov=True)
     assert_allclose(ts, ts_ref)
-    sigma = ts_to_sigma(ts, df=df, ts_asimov=ts)
+    sigma = ts_to_sigma(ts, df=df, asimov=True)
+    assert_allclose(sigma, sigma_ref)
+
+    df = 10
+    ts = sigma_to_ts(3, df=df, asimov=True)
+    sigma = ts_to_sigma(ts, df=df, asimov=True)
     assert_allclose(sigma, sigma_ref)
--- gammapy/stats/utils.py
+++ gammapy/stats/utils.py
@@ -1,13 +1,17 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import numpy as np
-from scipy.stats import ncx2
+from scipy.stats import ncx2, chi2
+from scipy.optimize import brentq
+
+from gammapy.utils.deprecation import deprecated_renamed_argument
 
 
 __all__ = ["sigma_to_ts", "ts_to_sigma"]
 
 
-def sigma_to_ts(n_sigma, df=1, n_sigma_asimov=0):
+@deprecated_renamed_argument("n_sigma_asimov", "asimov", "v2.0", arg_in_kwargs=True)
+def sigma_to_ts(n_sigma, df=1, asimov=False):
     """Convert number of sigma to delta ts.
 
     Assumes that the TS follows a chi2 distribution according to Wilks theorem [1].
@@ -22,12 +26,11 @@ def sigma_to_ts(n_sigma, df=1, n_sigma_asimov=0):
         Significance in number of sigma.
     df : int, optional
         Number of degree of freedom. Default is 1.
-    n_sigma_asimov : float, optional
-        Significance in number of sigma in the Asimov dataset
-        (in which counts are equal to the predicted counts).
+    asimov : bool, optional
+        Whether the number of sigma are calculated in an Asimov dataset.
         In that case the function applies the Wald test described in [2] and [3],
         where the TS of H1 under the H0 assumption is assumed to follow a non-central chi2 distribution.
-        Should only be used for sensitivity computations. Default is 0.
+        Should only be used for sensitivity computations. Default is False.
 
     Returns
     -------
@@ -43,12 +46,30 @@ def sigma_to_ts(n_sigma, df=1, n_sigma_asimov=0):
     .. [3] Cowan et al. (2011), European Physical Journal C, 71, 1554.
         doi:10.1140/epjc/s10052-011-1554-0.
     """
-    ts_asimov = n_sigma_asimov**2
-    p_value = ncx2.sf(n_sigma**2, df=1, nc=ts_asimov)
-    return ncx2.isf(p_value, df=df, nc=ts_asimov)
+    # invert ts_to_sigma step
+    p_value = chi2.sf(n_sigma**2, df=1)
+    q_med = chi2.isf(p_value, df=df)
+
+    # the simple case
+    if not asimov or df == 1:
+        return q_med
+
+    def f(nc):
+        return ncx2.ppf(0.5, df=df, nc=nc) - q_med
+
+    # f rises monotonically with nc
+    # if f(nc=0) is already positive there is no root to find
+    if f(0.0) >= 0:
+        return 0.0
+
+    # solve for nc = ts
+    ts = brentq(f, 0, q_med)
+
+    return ts
 
 
-def ts_to_sigma(ts, df=1, ts_asimov=0):
+@deprecated_renamed_argument("ts_asimov", "asimov", "v2.0", arg_in_kwargs=True)
+def ts_to_sigma(ts, df=1, asimov=False):
     """Convert delta ts to number of sigma.
 
     Assumes that the TS follows a chi2 distribution according to Wilks theorem [1].
@@ -63,12 +84,11 @@ def ts_to_sigma(ts, df=1, ts_asimov=0):
         Test statistic value.
     df : int, optional
         Number of degree of freedom. Default is 1.
-    ts_asimov : float, optional
-        TS value in the Asimov dataset
-        (in which counts equal to the predicted counts).
+    asimov : bool, optional
+        Whether the number of sigma are calculated in an Asimov dataset.
         In that case the function applies the Wald test described in [2] and [3],
-        and the TS is assumed to follow a non-central chi2 distribution.
-        Should only be used for sensitivity computations. Default is 0.
+        where the TS of H1 under the H0 assumption is assumed to follow a non-central chi2 distribution.
+        Should only be used for sensitivity computations. Default is False.
 
 
     Returns
@@ -85,5 +105,9 @@ def ts_to_sigma(ts, df=1, ts_asimov=0):
     .. [3] Cowan et al. (2011), European Physical Journal C, 71, 1554.
         doi:10.1140/epjc/s10052-011-1554-0.
     """
-    p_value = ncx2.sf(ts, df=df, nc=ts_asimov)
-    return np.sqrt(ncx2.isf(p_value, df=1, nc=ts_asimov))
+    q_med = ts
+    if asimov and df != 1:
+        q_med = ncx2.ppf(0.5, df=df, nc=ts)
+
+    p_value = chi2.sf(q_med, df=df)
+    return np.sqrt(chi2.isf(p_value, df=1))
